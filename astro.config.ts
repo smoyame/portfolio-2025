b42715bc838028c3bb80f67fde3fdaf6bb67004e
smoyame <smoyamedina@outlook.com>
2025-01-08 17:26:26 -0500

    Change back to mjs for now

--- astro.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-	output: 'static',
-	srcDir: './src',
-	publicDir: './static',
-	outDir: './public',
-	compressHTML: true
-});
