db402d2b32aefc5ff0030ea01dff551d0d0b5e30
smoyame <smoyamedina@outlook.com>
2025-01-08 17:32:42 -0500

    Test new env set up

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config';
 import { loadEnv } from "vite";
-import { dotenv } from 'dotenv'
 
 const { COMPRESS_HTML } = loadEnv(process.env.COMPRESS_HTML, process.cwd(), "");
+const compressHTML = process.env.COMPRESS_HTML
 
 export default defineConfig({
 	output: 'static',
 	srcDir: './src',
 	publicDir: './static',
 	outDir: './public',
-	compressHTML: COMPRESS_HTML
+	compressHTML: compressHTML
 });
