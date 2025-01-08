de929bd6de565ed347052ade5c840d1a9157116f
smoyame <smoyamedina@outlook.com>
2025-01-08 17:48:01 -0500

    Set back to default

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,8 @@
 import { defineConfig } from 'astro/config';
-import { loadEnv } from "vite";
-
-const { COMPRESS_HTML } = loadEnv(process.env.COMPRESS_HTML, process.cwd(), "");
-const compressHTML = process.env.COMPRESS_HTML
 
 export default defineConfig({
 	output: 'static',
-	srcDir: './src',
 	publicDir: './static',
 	outDir: './public',
-	compressHTML: compressHTML
+	compressHTML: false
 });
